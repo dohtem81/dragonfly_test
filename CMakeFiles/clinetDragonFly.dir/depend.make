0ed5b814483906a5cfd5b1bf2019c243fc9371d1
Piotr Pedziwiatr <pedziwitr.piotr@gmail.com>
2024-08-23 23:03:42 -0500

    initial commmit

---
+++ CMakeFiles/clinetDragonFly.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for clinetDragonFly.
+# This may be replaced when dependencies are built.
---
+++ client.cpp
@@ -0,0 +1,9 @@
+#include <hiredis/hiredis.h>
+#include <iostream>
+
+int main()
+{
+    std::cout << "hello hiredis test" << std::endl;
+
+    return 0;
+}
