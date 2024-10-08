26a2f3373e9e60e45e67ed2ea07cf0d5da6f6c19
Piotr Pedziwiatr <pedziwitr.piotr@gmail.com>
2024-10-07 23:37:39 -0500

    figured out where the problem was

--- client.cpp
+++ client.cpp
@@ -3,7 +3,24 @@
 
 int main()
 {
-    std::cout << "hello hiredis test" << std::endl;
+    std::cout << "hello hiredis test ultimate" << std::endl;
+
+    // connect to redis server
+    redisContext *c = redisConnect("127.0.0.1", 6379);
+    if (c == NULL || c->err) {
+        if (c) {
+            std::cerr << "Connection error: " << c->errstr << std::endl;
+            redisFree(c);
+        } else {
+            std::cerr << "Connection error: can't allocate redis context" << std::endl;
+        }
+        return 1;
+    }
+
+    std::cout << "Connected to Redis server" << std::endl;
+
+    // Clean up and free the context
+    redisFree(c);
 
     return 0;
 }
